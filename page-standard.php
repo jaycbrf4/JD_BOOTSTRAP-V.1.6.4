250ac268b8b60f1d9a87b0d8d9f4fc8b1eb0d255
Jay <jaycbrf@gmail.com>
2017-12-04 11:25:47 -0500

    Update page-standard
    Cleaned up code

--- page-standard.php
+++ page-standard.php
@@ -8,37 +8,46 @@
 
 <?php get_header(); ?>
 
-        <div id="content">
-          <div  class="container-fluid">
+<div id="main" class="clearfix" role="main">
+    <div id="content">
+
+    <!-- Hero Image -->
+     <div id="hero-image">
+       <h1 class="pagetitle">
+        <!-- Custom Field for Page Heading --><?php echo get_post_meta($post->ID, 'Page Name', true); ?>
+       </h1><!-- /.pagetitle -->
+      </div>
+    <!-- /#hero-image --> 
+
+    <!-- loop the loop -->       
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+      <section class="post_content">
+        <?php the_content(); ?>          
+      </section> <!-- end article section -->
+    </article> <!-- end article -->
+
+      <?php endwhile; ?>  
+      <?php else : ?>
+
+        <article id="post-not-found">
+          <div class="container">
             <div class="row">
-              <div id="main" class="clearfix" role="main">
+              <div class="col-sm-12">
+                <header>
+                  <h1><?php _e("Not Found", "JD_BOOTSTRAP"); ?></h1>
+                </header>
+                <section class="post_content">
+                  <p><?php _e("Sorry, but the requested resource was not found on this site.", "JD_BOOTSTRAP"); ?></p>
+                </section>
+              </div><!-- /.col-sm-12 -->
+            </div><!-- /.row -->
+          </div><!-- /.container -->          
+        </article>
 
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php endif; ?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-                  <section class="post_content">
-                    <?php the_content(); ?>
-                  </section> <!-- end article section -->
-                  </article> <!-- end article -->
-
-                <?php endwhile; ?>  
-
-                <?php else : ?>
-
-                  <article id="post-not-found">
-                    <header>
-                      <h1><?php _e("Not Found", "JD_BOOTSTRAP"); ?></h1>
-                    </header>
-                    <section class="post_content">
-                      <p><?php _e("Sorry, but the requested resource was not found on this site.", "JD_BOOTSTRAP"); ?></p>
-                    </section>
-                  </article>
-
-                <?php endif; ?>
-
-              </div><!--/main-->
-            </div><!--row-->
-          </div><!--/container-->
-        </div><!-- /. content -->
+  </div><!-- /. content -->
+</div><!--/main-->
 <?php get_footer(); ?>
-
