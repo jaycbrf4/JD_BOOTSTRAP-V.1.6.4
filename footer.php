dc6919ed306eda751b1114233c8fdb8778c1728b
Jay <jaycbrf@gmail.com>
2017-12-04 11:19:08 -0500

    Update footer.php
    Fixed Bootstrap code

--- footer.php
+++ footer.php
@@ -1,42 +1,46 @@
-      <!-- FOOTER -->
-  <footer id="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="clearfix" id="inner-footer">
-            <a class="scrollup"></a>         
-            <div id="widget-footer" class="clearfix">
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-              <?php endif; ?>
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-              <?php endif; ?>
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-              <?php endif; ?>
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer4') ) : ?>
-              <?php endif; ?>   
-            </div><!--/.widget-footer-->
-            <div id="tags" >
-              <div id="copyright" class="col-sm-6">
-              <!-- this is the option from the "Theme Options"  It will display the copyright information ONLY if it is present in the Theme Options-->
-                <?php $copyright_options = (array) get_option('copyright_options'); ?>
-                <?php $name = $copyright_options['name']; ?>
-                <?php if (0 < strlen( trim( $name ) ) ) { ?>
-                <p>Copyright &copy;  <?php echo date('Y'); ?> 
-                <?php echo $name; ?></p>
-                <?php } //end if  ?>
-              </div><!-- /#copyright -->
+<!-- FOOTER -->
+<footer id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="clearfix" id="inner-footer">
+          <a class="scrollup"></a>         
+          <div id="widget-footer" class="clearfix">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+            <?php endif; ?>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+            <?php endif; ?>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+            <?php endif; ?>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer4') ) : ?>
+            <?php endif; ?>   
+          </div><!--/#widget-footer-->
+          <div id="social-row" class="row">
+            <hr>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer5') ) : ?>
+            <?php endif; ?>
+          </div><!-- /#social-row -->
+          <div id="tags" >
+            <div id="copyright" class="col-sm-6">
+            <!-- this is the option from the "Theme Options"  It will display the copyright information ONLY if it is present in the Theme Options-->
+              <?php $copyright_options = (array) get_option('copyright_options'); ?>
+              <?php $name = $copyright_options['name']; ?>
+              <?php if (0 < strlen( trim( $name ) ) ) { ?>
+              <p>Copyright &copy;  <?php echo date('Y'); ?> 
+              <?php echo $name; ?></p>
+              <?php } //end if  ?>
+            </div><!-- /#copyright -->
 
-              <div id="slogan" class="col-sm-6 text-right">
-                <p><?= get_bloginfo('name'); ?> - "<?= get_bloginfo('description'); ?>"</p>
-              </div>  <!--/#slogan-->  
-            </div><!--/.tags-->
-          </div> <!--/#inner-footer-->
-        </div><!--/.col-->
-      </div> <!-- .row -->              
-    </div><!-- /.container -->
-  <p style="display:none;">Site Designed and Built by: <a href="http://hudsonvalleywebdesign.net" target="_blank">Jay Deutsch</a></p>
-  <?php wp_footer(); ?>
-  </footer>
+            <div id="credit" class="col-sm-6 text-right">
+              <p><i class="fa fa-tv"></i> Site Designed and Built by: <a href="http://hudsonvalleywebdesign.net" target="_blank">Hudson Valley Web Design</a></p>
+            </div>  <!--/#credit-->  
+          </div><!--/.tags-->
+        </div> <!--/#inner-footer-->
+      </div><!--/.col-->
+    </div> <!-- .row -->              
+  </div><!-- /.container -->
+<?php wp_footer(); ?>
+</footer>
   <?php $options = get_option( 'GA_options' ); ?>
   <?php $ID = $options['wp_textbox']; ?>
   <?php if (0 < strlen( trim( $ID ) ) ) { ?>
